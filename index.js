d294cfc218dcf47510fa465b8f8f1e22b4afd557
leonbyrdjr <leonbyrdjr@Leons-MacBook-Pro.local>
2022-01-25 17:44:43 -0600

    commit tree 0b067697fff73ff1de0141e55f825430d9b0640e

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+    console.log(add());
+}
+function subtract() {
+    console.log(subtract());
+}
+function multiply() {
+    console.log(multiply());
+}
+function divide() {
+    console.log(divide());
+}
+function add(x, y){
+    return (x+ y);
+}
+function subtract(x, y) {
+    return (x - y);
+}
+function multiply(x, y) {
+    return ( x * y);
+}
+function divide(x, y) {
+    return (x / y);
+}
+function increment(x) {
+    return (x + 1);
+}
+function decrement(x) {
+    return (x - 1);
+}
+function makeInt(x) {
+    return parseInt(x, 10);
+}
+function preserveDecimal(x) {
+    return parseFloat(x);
+}
